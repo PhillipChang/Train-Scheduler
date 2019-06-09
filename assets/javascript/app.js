ee23e094a4cbc0f8bd4fa74a8e8e921d9fcf0a27
Phillip Chang <phillip-chang@live.com>
2019-06-09 13:30:57 -0700

    Create jumbotron and add train logo

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// Firebase 
+var firebaseConfig = {
+    apiKey: "AIzaSyCLQan-fSqXFXK2AyepiUuhLQ8jMi1Aimo",
+    authDomain: "train-scheduler-7fdc7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7fdc7.firebaseio.com",
+    projectId: "train-scheduler-7fdc7",
+    storageBucket: "train-scheduler-7fdc7.appspot.com",
+    messagingSenderId: "655771507189",
+    appId: "1:655771507189:web:67330b3a1cf7425f"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
